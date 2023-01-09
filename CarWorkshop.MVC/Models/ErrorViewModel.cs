d6bd46717141cf89da65ccd73c934888c7829bfb
Jakub Kozera <kozerakuba@gmail.com>
2023-01-09 08:16:25 +0100

    Add project files.

---
+++ CarWorkshop.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarWorkshop.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
