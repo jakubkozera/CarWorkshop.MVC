949f5511034c11788388e8b208dc9f3f3de95ac3
Jakub Kozera <kozerakuba@gmail.com>
2023-01-25 20:22:54 +0100

    2.3 Model danych

---
+++ CarWorkshop.Domain/Entities/CarWorkshop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Domain.Entities
+{
+    public class CarWorkshop
+    {
+        public required int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public CarWorkshopContactDetails ContactDetails { get; set; } = default!;
+
+        public string EncodedName { get; private set; } = default!;
+
+        public void EncodeName() => EncodedName = Name.ToLower().Replace(" ", "-");
+    }
+}
---
+++ CarWorkshop.Domain/Entities/CarWorkshopContactDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace CarWorkshop.Domain.Entities
+{
+    public class CarWorkshopContactDetails
+    {
+        public string? PhoneNumber { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+    }
+}
