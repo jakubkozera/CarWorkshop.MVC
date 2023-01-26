8bc41c8f38003a9ee0091ff1de33778c64390ef2
Jakub Kozera <kozerakuba@gmail.com>
2023-01-26 19:43:10 +0100

    3.2 Instalacja EF

---
+++ CarWorkshop.Infrastructure/Migrations/20230126064207_Init.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarWorkshop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarWorkshops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ContactDetailsPhoneNumber = table.Column<string>(name: "ContactDetails_PhoneNumber", type: "nvarchar(max)", nullable: true),
+                    ContactDetailsStreet = table.Column<string>(name: "ContactDetails_Street", type: "nvarchar(max)", nullable: true),
+                    ContactDetailsCity = table.Column<string>(name: "ContactDetails_City", type: "nvarchar(max)", nullable: true),
+                    ContactDetailsPostalCode = table.Column<string>(name: "ContactDetails_PostalCode", type: "nvarchar(max)", nullable: true),
+                    EncodedName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarWorkshops", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarWorkshops");
+        }
+    }
+}
---
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Infrastructure.Persistence
+{
+    public class CarWorkshopDbContext : DbContext
+    {
+        public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarWorkshopDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+                .OwnsOne(c => c.ContactDetails);
+        }
+    }
+}
