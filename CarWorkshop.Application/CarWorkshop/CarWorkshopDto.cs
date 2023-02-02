f7e9a3e799ad95ce9b5004c8ce3040fed0281539
Jakub Kozera <kozerakuba@gmail.com>
2023-02-02 20:48:49 +0100

    4.4. Podstawowa walidacja

--- CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
+++ CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace CarWorkshop.Application.CarWorkshop
 {
     public class CarWorkshopDto
     {
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
         public string Name { get; set; } = default!;
+      
+        [Required(ErrorMessage = "Please insert description")]
         public string? Description { get; set; }
         public string? About { get; set; }
+       
+        [StringLength(12, MinimumLength = 8)]
         public string? PhoneNumber { get; set; }
         public string? Street { get; set; }
         public string? City { get; set; }
--- CarWorkshop.MVC/Controllers/CarWorkshopController.cs
+++ CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -21,6 +21,11 @@ namespace CarWorkshop.MVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CarWorkshopDto carWorkshop)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(carWorkshop);
+
+            }
             await _carWorkshopService.Create(carWorkshop);
             return RedirectToAction(nameof(Create)); //TODO: refactor
         }
