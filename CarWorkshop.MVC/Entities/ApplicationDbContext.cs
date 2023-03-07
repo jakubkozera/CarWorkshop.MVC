fdf265dd5000644ec8bbaa38b1355908afc8f9d2
Jakub Kozera <kozerakuba@gmail.com>
2023-03-07 07:41:35 +0100

    EF Context

---
+++ CarWorkshop.MVC/Entities/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshop.MVC.Entities
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<CarWorkshopEntity> CarWorkshops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarWorkshopMVC;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ CarWorkshop.MVC/Entities/CarWorkshopEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace CarWorkshop.MVC.Entities
+{
+    public class CarWorkshopEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Desciption { get; set; }
+
+        public string Address { get; set; } = default!;
+        public string? PhoneNumber { get; set; }
+    }
+}
