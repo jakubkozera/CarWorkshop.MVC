28ba422c70b71a26db4c7301ddea1f8ed297d554
Jakub Kozera <kozerakuba@gmail.com>
2023-05-29 16:50:45 +0200

    1.1 CarWorkshopTests

---
+++ CarWorkshop.Domain.Tests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,37 @@
+﻿using CarWorkshop;
+using FluentAssertions;
+
+namespace CarWorkshop.Domain.Tests.Entities
+{
+    public class CarWorkshopTests
+    {
+        [Fact]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            // Arrange
+            var carWorkshop = new Domain.Entities.CarWorkshop();
+            carWorkshop.Name = "Test Workshop";
+
+            // Act
+            carWorkshop.EncodeName();
+
+            // Assert
+            carWorkshop.EncodedName.Should().Be("test-workshop");
+        }
+
+        [Fact]
+        public void EncodeName_ShouldThrowException_WhenNameIsNull()
+        {
+            // Arrange
+            var carWorkshop = new Domain.Entities.CarWorkshop();
+
+            // Act
+            Action action = () => carWorkshop.EncodeName();
+
+            // Assert
+            action.Invoking(a => a.Invoke())
+                .Should().Throw<NullReferenceException>();
+        }
+
+    }
+}
