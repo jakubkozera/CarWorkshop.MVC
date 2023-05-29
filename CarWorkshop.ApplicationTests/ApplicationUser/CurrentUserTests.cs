7d2b6776fca13ece8edc02d829f0071052a538df
Jakub Kozera <kozerakuba@gmail.com>
2023-05-29 16:51:39 +0200

    1.2 CurrentUserTests

---
+++ CarWorkshop.ApplicationTests/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,40 @@
+﻿using CarWorkshop.Application.CarWorkshop;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CarWorkshop.ApplicationTests.ApplicationUser
+{
+    public class CurrentUserTests
+    {
+        [Fact]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue()
+        {
+            // Arrange
+            var currentUser = new CurrentUser("1", "test@example.com", new List<string> { "Admin", "User" });
+
+            // Act
+            var isInRole = currentUser.IsInRole("Admin");
+
+            // Assert
+            isInRole.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsInRole_WithNonMatchingRole_ShouldReturnFalse()
+        {
+            // Arrange
+            var currentUser = new CurrentUser("1", "test@example.com", new List<string> { "Admin", "User" });
+
+            // Act
+            var isInRole = currentUser.IsInRole("Manager");
+
+            // Assert
+            isInRole.Should().BeFalse();
+        }
+    }
+}
