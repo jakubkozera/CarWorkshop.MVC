9a89b21570afa18f06096b04fd497044f45f3365
Jakub Kozera <kozerakuba@gmail.com>
2023-05-30 06:43:46 +0200

    1.2 CurrentUserTests

---
+++ CarWorkshop.Application.Tests/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using CarWorkshop.Application.CarWorkshop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CarWorkshop.Application.CarWorkshop.Tests
+{
+    public class CurrentUserTests
+    {
+        [Fact()]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue()
+        {
+            // arrange
+
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+            // act
+
+            var isInRole  = currentUser.IsInRole("Admin");
+
+            // assert
+
+            isInRole.Should().BeTrue();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNonMatchingRole_ShouldReturnFalse()
+        {
+            // arrange
+
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+            // act
+
+            var isInRole = currentUser.IsInRole("Manager");
+
+            // assert
+
+            isInRole.Should().BeFalse();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNonMatchingCaseRole_ShouldReturnFalse()
+        {
+            // arrange
+
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+            // act
+
+            var isInRole = currentUser.IsInRole("admin");
+
+            // assert
+
+            isInRole.Should().BeFalse();
+        }
+    }
+}
