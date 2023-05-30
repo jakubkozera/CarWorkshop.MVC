48b127b8435100f01edc96272c039d7e78d2aef5
Jakub Kozera <kozerakuba@gmail.com>
2023-05-30 07:03:29 +0200

    7.4 UserContextTests

---
+++ CarWorkshop.Application.Tests/ApplicationUser/UserContextTests.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using CarWorkshop.Application.ApplicationUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+using FluentAssertions;
+
+namespace CarWorkshop.Application.ApplicationUser.Tests
+{
+    public class UserContextTests
+    {
+        [Fact()]
+        public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+        {
+            // arrange
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Email, "test@example.com"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            // act
+
+            var currentUser = userContext.GetCurrentUser();
+
+            // arrange 
+
+            currentUser.Should().NotBeNull();
+            currentUser!.Id.Should().Be("1");
+            currentUser.Email.Should().Be("test@example.com");
+            currentUser.Roles.Should().ContainInOrder("Admin", "User");
+        }
+    }
+}
