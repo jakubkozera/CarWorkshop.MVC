54345f3c53938bbcf4f042f58237913a47dd24a4
Jakub Kozera <kozerakuba@gmail.com>
2023-05-30 07:32:48 +0200

    7.6 Testy walidatora

---
+++ CarWorkshop.Application.Tests/CarWorkshopService/Commands/CreateCarWorkshopServiceCommandValidatorTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using CarWorkshop.Application.CarWorkshopService.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.TestHelper;
+
+namespace CarWorkshop.Application.CarWorkshopService.Commands.Tests
+{
+    public class CreateCarWorkshopServiceCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldNotHaveValidationError()
+        {
+            // arrange
+
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "100 PLN",
+                Description = "Description",
+                CarWorkshopEncodedName = "workshop1"
+            };
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            //assert
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact()]
+        public void Validate_WithInvalidCommand_ShouldHaveValidationErrors()
+        {
+            // arrange
+
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "",
+                Description = "",
+                CarWorkshopEncodedName = null
+            };
+
+            // act
+
+            var result = validator.TestValidate(command);
+
+            //assert
+
+            result.ShouldHaveValidationErrorFor(c => c.Cost);
+            result.ShouldHaveValidationErrorFor(c => c.Description);
+            result.ShouldHaveValidationErrorFor(c => c.CarWorkshopEncodedName);
+        }
+    }
+}
